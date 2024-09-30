9b4012d402402e03f1d2dcdc1098607f3fa2607b
KantaKan <140788074+KantaKan@users.noreply.github.com>
2024-09-30 19:07:30 +0700

    Update geminiService.js

--- services/geminiService.js
+++ services/geminiService.js
@@ -1,4 +1,3 @@
-// geminiService.js
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const calorieUtils = require("../utils/calorieUtils");
 const fetch = require("node-fetch");
@@ -10,25 +9,34 @@ const geminiService = {
       const client = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
       const model = client.getGenerativeModel({ model: "gemini-1.5-pro" });
 
-      const calorieResult = await model.generateContent(`ใน ${foodName} มีประมาณกี่แคล ตอบเป็นภาษาอังกฤษ ประมาณมาได้เลยไม่ต้องห่วง`);
-      const caloriesText = calorieResult.response.text();
-      console.log("Calorie Information:", caloriesText);
-      const calorieInfo = calorieUtils.extractCalorieInfo(caloriesText);
+      
+      const prompt = `${foodName} has how many calories? Answer with this JSON format: { "calories": , "forImageSearch": }`; 
 
-      const unsplashResponse = await fetch(`https://api.unsplash.com/search/photos?query=${foodName}&client_id=${process.env.UNSPLASH_API_KEY}`);
+      const result = await model.generateContent(prompt);
+      const responseText = result.response.text();
+      console.log("Gemini Response:", responseText);
+
+      
+      const data = JSON.parse(responseText); 
+
+   
+      const calories = data.calories;
+      const imageSearchKeyword = data.forImageSearch;
+
+     
+      const unsplashResponse = await fetch(`https://api.unsplash.com/search/photos?query=${imageSearchKeyword}&client_id=${process.env.UNSPLASH_API_KEY}`);
       const unsplashData = await unsplashResponse.json();
 
       let imageUrl = null;
       if (unsplashData.results && unsplashData.results.length > 0) {
         imageUrl = unsplashData.results[0].urls.regular;
       } else {
-        console.log("No image found on Unsplash for", foodName);
-
-        imageUrl = "https://res.cloudinary.com/dgtdr85d8/image/upload/v1727696615/unnamed_ehkjkw.png";
+        console.log("No image found on Unsplash for", imageSearchKeyword);
+        imageUrl = "https://res.cloudinary.com/dgtdr85d8/image/upload/v1727696615/unnamed_ehkjkw.png"; 
       }
 
       return {
-        calories: calorieInfo,
+        calories: calories,
         imageUrl: imageUrl,
       };
     } catch (error) {
